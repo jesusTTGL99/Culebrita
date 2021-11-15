7214671028b062a5322977172b2b4a03ca656c0c
jesusTTGL99 <jegonzaleza.18@est.ucab.edu.ve>
2021-11-15 18:24:56 -0400

    Creando la comida

--- culebrita.py
+++ culebrita.py
@@ -1,8 +1,36 @@
 import turtle
+import time
+import random
+
+def arriba() :
+    serpiente.direction = "up"
+
+def abajo() :
+    serpiente.direction = "down"
+
+def derecha() :
+    serpiente.direction = "right"
+
+def izquierda() :
+    serpiente.direction = "left"
+
+def movimiento() :
+    if serpiente.direction == "up" :
+        y = serpiente.ycor()
+        serpiente.sety(y + 20)
+    if serpiente.direction == "down" :
+        y = serpiente.ycor()
+        serpiente.sety(y - 20)
+    if serpiente.direction == "right" :
+        x = serpiente.xcor()
+        serpiente.setx(x + 20)
+    if serpiente.direction == "left" :
+        x = serpiente.xcor()
+        serpiente.setx(x - 20)
 
 s = turtle.Screen()
 s.setup(650,650)
-s.bgcolor("gray")
+s.bgcolor("black")
 s.title("culebrita")
 
 serpiente = turtle.Turtle()
@@ -11,5 +39,27 @@ serpiente.shape("square")
 serpiente.penup()
 serpiente.direction = "stop"
 serpiente.color("red")
+
+comida = turtle.Turtle()
+comida.shape("circle")
+comida.color("orange")
+comida.penup()
+comida.goto(100,100)
+comida.speed(0)
+
+s.listen()
+s.onkeypress(arriba, "Up")
+s.onkeypress(abajo, "Down")
+s.onkeypress(derecha, "Right")
+s.onkeypress(izquierda, "Left")
+
+while True :
+    s.update()
+    if serpiente.distance(comida) < 20 :
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        comida.goto(x, y)
+    movimiento()
+    time.sleep(0.2)
 
 turtle.done()
